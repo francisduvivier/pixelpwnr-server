c5c3700f47e75139302ed91eb29849792547d4b5
Francis Duvivier <francisduvivier@gmail.com>
2023-09-29 08:48:31 +0200

    remove sleep from as_bytes

--- render/src/pixmap.rs
+++ render/src/pixmap.rs
@@ -122,7 +122,6 @@ impl Pixmap {
     /// This data may be used to send to the GPU, as raw texture buffer, for
     /// rendering.
     pub fn as_bytes<'me>(&'me mut self) -> &[u8] {
-        sleep(Duration::from_millis(100));
         let map = &self.map;
 
         let len = map.len() * 4;
